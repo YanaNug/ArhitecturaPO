6ec20d7c76990bd5e5d227cb51c19577fe782de6
Yana <yana229910nug@yandex.ru>
2023-10-18 10:15:07 +0300

    9

---
+++ 9/ERD-диаграмма.sql
@@ -0,0 +1,9 @@
+ALTER TABLE `Order` DROP FOREIGN KEY `Order_fk0`;
+
+ALTER TABLE `Order` DROP FOREIGN KEY `Order_fk1`;
+
+ALTER TABLE `Cloud` DROP FOREIGN KEY `Cloud_fk0`;
+
+ALTER TABLE `Cloud` DROP FOREIGN KEY `Cloud_fk1`;
+
+ALTER TABLE `Cloud` DROP FOREIGN KEY `Cloud_fk2`;
